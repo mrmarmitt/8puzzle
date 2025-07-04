880c4e9e1737f72a180e9a350eaf7392d89d04bd
Mateus Rufino Marmitt <mrmarmitt@gmail.com>
2025-07-04 16:04:07 -0300

    moving core to game

--- src/platform/terminal/screen/GameScreen.h
+++ src/platform/terminal/screen/GameScreen.h
@@ -1,21 +1,20 @@
 #pragma once
 
-// #include <chrono>
 #include <memory>
 
 #include "../../../8puzzle/engine/IScreen.h"
-#include "8puzzle/core/Board.h"
+#include "8puzzle/game/core/Board.h"
 
 class GamePlayService;
 class GameScreen final : public IScreen {
     std::shared_ptr<GamePlayService> m_gamePlayService;
 
-    // Board m_board;
-    Board m_board = Board({
-        Tile(1, "1"), Tile(2, "2"), Tile(3, "3"),
-        Tile(4, "4"), Tile(5, "5"), Tile(6, "6"),
-        Tile(7, "7"), Tile(0, " "), Tile(8, "8")
-    });
+    Board m_board;
+    // Board m_board = Board({
+    //     Tile(1, "1"), Tile(2, "2"), Tile(3, "3"),
+    //     Tile(4, "4"), Tile(5, "5"), Tile(6, "6"),
+    //     Tile(7, "7"), Tile(0, " "), Tile(8, "8")
+    // });
     [[nodiscard]] std::string printBoard() const;
 
 public:
