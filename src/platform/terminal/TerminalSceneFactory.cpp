eec2ce6e8281220fd4582a45b214006ef111d9b8
Mateus Rufino Marmitt <mrmarmitt@gmail.com>
2025-07-08 10:48:59 -0300

    rename screen to scene

--- src/platform/terminal/TerminalSceneFactory.cpp
+++ src/platform/terminal/TerminalSceneFactory.cpp
@@ -2,13 +2,13 @@
 
 #include "../../8puzzle/engine/SceneContainer.h"
 #include "8puzzle/game/state/StateEnum.h"
-#include "screen/ExitScene.h"
-#include "screen/GameOverScene.h"
-#include "screen/GameScene.h"
-#include "screen/InitialScene.h"
-#include "screen/IntroductionScene.h"
-#include "screen/MenuScene.h"
-#include "screen/RecordScene.h"
+#include "scene/ExitScene.h"
+#include "scene/GameOverScene.h"
+#include "scene/GameScene.h"
+#include "scene/InitialScene.h"
+#include "scene/IntroductionScene.h"
+#include "scene/MenuScene.h"
+#include "scene/RecordScene.h"
 
 void TerminalSceneFactory::populateTerminalScreens(SceneContainer& container, GameRouter& gameRouter) {
     static InitialScene initialScreen(gameRouter);
